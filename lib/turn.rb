3262eaa34e8ef9966bd1c0ae468f0f13335880fb
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 07:06:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index <= 8
+  if index <= 9
     if position_taken?(board, index) == false
       return true
     else
