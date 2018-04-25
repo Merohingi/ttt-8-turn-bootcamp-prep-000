25c1a6912813c2161597e633ed5d9bd3a503525b
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 07:05:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ end
 def turn(board)
   loop do
     puts "Please enter 1-9:"
-    input = gets.chomp
+    input = gets.strip
     index = input_to_index(input)
     if (valid_move?(board, index))
       move(board, index)
